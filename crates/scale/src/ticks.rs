d4fdfa1fad7c92f74d7b6dfc6ed32251935cf549
bourbonut <ben.bourbon06@gmail.com>
2026-03-26 18:16:41 +0100

    Fix `ticks.rs` and add tests

--- crates/scale/src/ticks.rs
+++ crates/scale/src/ticks.rs
@@ -18,29 +18,31 @@ fn tick_spec(start: f32, stop: f32, count: usize) -> [f32; 3] {
         1
     };
 
-    let [i1, i2, inc] = if power < 0. {
-        let inc = 10_f32.powf(-power) / factor as f32;
-        let mut i1 = (start * inc).round();
-        let mut i2 = (stop * inc).round();
+    let mut inc;
+    let mut i1;
+    let mut i2;
+    if power < 0. {
+        inc = 10_f32.powf(-power) / factor as f32;
+        i1 = (start * inc).round();
+        i2 = (stop * inc).round();
         if i1 / inc < start {
-            i1 += 1_f32;
+            i1 += 1.;
         }
         if i2 / inc > stop {
-            i2 -= 1_f32;
+            i2 -= 1.;
         }
-        [i1, i2, -inc]
+        inc = -inc;
     } else {
-        let inc = 10_f32.powf(power) / factor as f32;
-        let mut i1 = (start * inc).round();
-        let mut i2 = (stop * inc).round();
-        if i1 / inc < start {
-            i1 += 1_f32;
+        inc = 10_f32.powf(power) * factor as f32;
+        i1 = (start / inc).round();
+        i2 = (stop / inc).round();
+        if i1 * inc < start {
+            i1 += 1.;
         }
-        if i2 / inc > stop {
-            i2 -= 1_f32;
+        if i2 * inc > stop {
+            i2 -= 1.;
         }
-        [i1, i2, inc]
-    };
+    }
     if i2 < i1 && count < 2 {
         return tick_spec(start, stop, count * 2);
     }
@@ -66,12 +68,101 @@ pub fn ticks(start: f32, stop: f32, count: usize) -> Vec<f32> {
     let n = (i2 - i1 + 1.) as usize;
     match (reverse, inc < 0.) {
         (true, true) => (0..n).map(|i| (i2 - i as f32) / -inc).collect(),
-        (true, false) => (0..n).map(|i| (i2 - i as f32) / inc).collect(),
+        (true, false) => (0..n).map(|i| (i2 - i as f32) * inc).collect(),
         (false, true) => (0..n).map(|i| (i1 + i as f32) / -inc).collect(),
-        (false, false) => (0..n).map(|i| (i1 + i as f32) / inc).collect(),
+        (false, false) => (0..n).map(|i| (i1 + i as f32) * inc).collect(),
     }
 }
 
 pub fn tick_increment(start: f32, stop: f32, count: usize) -> f32 {
     tick_spec(start, stop, count)[2]
+}
+
+mod tests {
+
+    #[rustfmt::skip]
+    #[test]
+    fn test_ticks_1() {
+        assert_eq!(super::ticks(0., 2.2, 3), [0., 1., 2.]);
+        assert_eq!(super::ticks(0., 1., 10), [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]);
+        assert_eq!(super::ticks(0., 1., 9), [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]);
+        assert_eq!(super::ticks(0., 1., 8), [0.0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]);
+        assert_eq!(super::ticks(0., 1., 7), [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]);
+        assert_eq!(super::ticks(0., 1., 6), [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]);
+        assert_eq!(super::ticks(0., 1., 5), [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]);
+        assert_eq!(super::ticks(0., 1., 4), [0.0, 0.2, 0.4, 0.6, 0.8, 1.0]);
+        assert_eq!(super::ticks(0., 1., 3), [0.0, 0.5, 1.0]);
+        assert_eq!(super::ticks(0., 1., 2), [0.0, 0.5, 1.0]);
+        assert_eq!(super::ticks(0., 1., 1), [0.0, 1.0]);
+        assert_eq!(super::ticks(0., 10., 10), [0., 1., 2., 3., 4., 5., 6., 7., 8., 9., 10.]);
+        assert_eq!(super::ticks(0., 10., 9), [0., 1., 2., 3., 4., 5., 6., 7., 8., 9., 10.]);
+        assert_eq!(super::ticks(0., 10., 8), [0., 1., 2., 3., 4., 5., 6., 7., 8., 9., 10.]);
+        assert_eq!(super::ticks(0., 10., 7), [0., 2., 4., 6., 8., 10.]);
+        assert_eq!(super::ticks(0., 10., 6), [0., 2., 4., 6., 8., 10.]);
+        assert_eq!(super::ticks(0., 10., 5), [0., 2., 4., 6., 8., 10.]);
+        assert_eq!(super::ticks(0., 10., 4), [0., 2., 4., 6., 8., 10.]);
+        assert_eq!(super::ticks(0., 10., 3), [0., 5., 10.]);
+        assert_eq!(super::ticks(0., 10., 2), [0., 5., 10.]);
+        assert_eq!(super::ticks(0., 10., 1), [0., 10.]);
+        assert_eq!(super::ticks(-10., 10., 10), [-10., -8., -6., -4., -2., 0., 2., 4., 6., 8., 10.]);
+        assert_eq!(super::ticks(-10., 10., 9), [-10., -8., -6., -4., -2., 0., 2., 4., 6., 8., 10.]);
+        assert_eq!(super::ticks(-10., 10., 8), [-10., -8., -6., -4., -2., 0., 2., 4., 6., 8., 10.]);
+        assert_eq!(super::ticks(-10., 10., 7), [-10., -8., -6., -4., -2., 0., 2., 4., 6., 8., 10.]);
+        assert_eq!(super::ticks(-10., 10., 6), [-10., -5., 0., 5., 10.]);
+        assert_eq!(super::ticks(-10., 10., 5), [-10., -5., 0., 5., 10.]);
+        assert_eq!(super::ticks(-10., 10., 4), [-10., -5., 0., 5., 10.]);
+        assert_eq!(super::ticks(-10., 10., 3), [-10., -5., 0., 5., 10.]);
+        assert_eq!(super::ticks(-10., 10., 2), [-10., 0., 10.]);
+        assert_eq!(super::ticks(-10., 10., 1), [0.]);
+    }
+
+    #[rustfmt::skip]
+    #[test]
+    fn test_ticks_2() {
+        assert_eq!(super::ticks(1., 0., 10), super::ticks(0., 1., 10).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(1., 0., 9), super::ticks(0., 1., 9).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(1., 0., 8), super::ticks(0., 1., 8).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(1., 0., 7), super::ticks(0., 1., 7).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(1., 0., 6), super::ticks(0., 1., 6).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(1., 0., 5), super::ticks(0., 1., 5).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(1., 0., 4), super::ticks(0., 1., 4).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(1., 0., 3), super::ticks(0., 1., 3).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(1., 0., 2), super::ticks(0., 1., 2).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(1., 0., 1), super::ticks(0., 1., 1).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., 0., 10), super::ticks(0., 10., 10).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., 0., 9), super::ticks(0., 10., 9).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., 0., 8), super::ticks(0., 10., 8).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., 0., 7), super::ticks(0., 10., 7).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., 0., 6), super::ticks(0., 10., 6).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., 0., 5), super::ticks(0., 10., 5).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., 0., 4), super::ticks(0., 10., 4).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., 0., 3), super::ticks(0., 10., 3).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., 0., 2), super::ticks(0., 10., 2).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., 0., 1), super::ticks(0., 10., 1).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., -10., 10), super::ticks(-10., 10., 10).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., -10., 9), super::ticks(-10., 10., 9).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., -10., 8), super::ticks(-10., 10., 8).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., -10., 7), super::ticks(-10., 10., 7).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., -10., 6), super::ticks(-10., 10., 6).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., -10., 5), super::ticks(-10., 10., 5).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., -10., 4), super::ticks(-10., 10., 4).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., -10., 3), super::ticks(-10., 10., 3).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., -10., 2), super::ticks(-10., 10., 2).into_iter().rev().collect::<Vec<f32>>());
+        assert_eq!(super::ticks(10., -10., 1), super::ticks(-10., 10., 1).into_iter().rev().collect::<Vec<f32>>());
+    }
+
+    #[rustfmt::skip]
+    #[test]
+    fn test_ticks_3() {
+        assert_eq!(super::ticks(0.98, 1.14, 10), [0.98, 1., 1.02, 1.04, 1.06, 1.08, 1.1, 1.12, 1.14]);
+        assert_eq!(super::ticks(1., 364., 1), [200.]);
+        assert_eq!(super::ticks(1., 499., 1), [200., 400.]);
+        assert_eq!(super::ticks(364., 1., 1), [200.]);
+        assert_eq!(super::ticks(0.001, 0.364, 1), [0.2]);
+        assert_eq!(super::ticks(0.364, 0.001, 1), [0.2]);
+        assert_eq!(super::ticks(-1., -364., 1), [-200.]);
+        assert_eq!(super::ticks(-364., -1., 1), [-200.]);
+        assert_eq!(super::ticks(-0.001, -0.364, 1), [-0.2]);
+        assert_eq!(super::ticks(-0.364, -0.001, 1), [-0.2]);
+    }
 }
