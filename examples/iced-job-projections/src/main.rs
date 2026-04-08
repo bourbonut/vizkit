95e45a8cc1780ea5ad4f4297d23ec54c73a9850c
bourbonut <ben.bourbon06@gmail.com>
2026-04-08 14:42:16 +0200

    Refactor `iced-job-projections` example

--- examples/iced-job-projections/src/main.rs
+++ examples/iced-job-projections/src/main.rs
@@ -1,12 +1,11 @@
 mod data;
-use core::f32;
 use std::str::FromStr;
 
-use iced::{
-    Element,
-    widget::{canvas, text},
+use iced::{Element, widget::canvas};
+use polars::{
+    frame::DataFrame,
+    prelude::{ChunkedArray, Column, PolarsDataType},
 };
-use polars::{frame::DataFrame, prelude::Column};
 use vizkit::scale::{ScaleContinuous, ScaleOrdinal};
 
 use crate::data::load_transform_data;
@@ -18,59 +17,60 @@ struct Margin {
     left: f32,
 }
 
+/// Finds the min in a `Column`
 fn min(column: &Column) -> Result<f32, Box<dyn std::error::Error>> {
     Ok(column.min_reduce()?.into_value().try_extract::<f32>()?)
 }
 
+/// Finds the max in a `Column`
 fn max(column: &Column) -> Result<f32, Box<dyn std::error::Error>> {
     Ok(column.max_reduce()?.into_value().try_extract::<f32>()?)
 }
 
+/// Converts a `ChunkedArray` into `Vec<T>`
+fn into_vec<'a, T: std::default::Default, U: PolarsDataType>(
+    chunk_arr: &'a ChunkedArray<U>,
+    f: impl Fn(U::Physical<'a>) -> T,
+) -> Vec<T> {
+    chunk_arr
+        .iter()
+        .map(|x| x.map(|v| f(v)).unwrap_or_default())
+        .collect()
+}
+
 struct Plot {
+    /// Radius domain for circles
     radius_domain: [f32; 2],
+    /// X domain
     x_domain: [f32; 2],
+    /// Margin dimensions
     margin: Margin,
+    /// Turnover values
     turnover: Vec<f32>,
+    /// Median wage values
     median_wage: Vec<f32>,
+    /// Openings values
     openings: Vec<f32>,
+    /// Sector cat values
     sector_cat: Vec<String>,
 }
 
 impl Plot {
     fn new(df: &DataFrame, margin: Margin) -> Result<Self, Box<dyn std::error::Error>> {
-        let xmax = df["turnover"]
-            .max_reduce()?
-            .into_value()
-            .try_extract::<f32>()?;
-
-        println!("{:?}", df["turnover"].dtype());
-
         Ok(Self {
             radius_domain: [min(&df["openings"])?, max(&df["openings"])?],
-            x_domain: [0., xmax],
-            turnover: df["turnover"]
-                .f64()?
-                .into_iter()
-                .map(|x| x.map(|v| v as f32).unwrap_or_default())
-                .collect::<Vec<f32>>(),
-            median_wage: df["Median Wage 2018"]
-                .f64()?
-                .into_iter()
-                .map(|x| x.map(|v| v as f32).unwrap_or_default())
-                .collect::<Vec<f32>>(),
-            openings: df["openings"]
-                .f64()?
-                .into_iter()
-                .map(|x| x.map(|v| v as f32).unwrap_or_default())
-                .collect::<Vec<f32>>(),
-            sector_cat: df["sector_cat"]
-                .str()?
-                .into_iter()
-                .map(|x| x.map(|v| v.to_string()).unwrap_or_default())
-                .collect::<Vec<String>>(),
+            x_domain: [0., max(&df["turnover"])?],
+            turnover: into_vec(df["turnover"].f64()?, |v| v as f32),
+            median_wage: into_vec(df["Median Wage 2018"].f64()?, |v| v as f32),
+            openings: into_vec(df["openings"].f64()?, |v| v as f32),
+            sector_cat: into_vec(df["sector_cat"].str()?, |v| v.to_string()),
             margin,
         })
     }
+}
+
+fn line(from: [f32; 2], to: [f32; 2]) -> canvas::Path {
+    canvas::Path::line(from.into(), to.into())
 }
 
 enum Message {}
@@ -89,6 +89,9 @@ impl<Message> canvas::Program<Message> for Plot {
         let mut frame = canvas::Frame::new(renderer, bounds.size());
         let width = bounds.width;
         let height = bounds.height;
+
+        let white = iced::Color::WHITE;
+        let stroke_white = canvas::Stroke::default().with_color(white);
 
         let radius = ScaleContinuous::sqrt()
             .domain(self.radius_domain.clone())
@@ -118,79 +121,68 @@ impl<Message> canvas::Program<Message> for Plot {
                 "#1b9e77", "#d95f02", "#7570b3", "#e7298a", "#66a61e", "#e6ab02", "#a6761d",
             ]);
 
-        // x axis
-        let x_axis = canvas::Path::line(
-            [self.margin.left, height - self.margin.bottom].into(),
-            [width - self.margin.right, height - self.margin.bottom].into(),
-        );
-        frame.stroke(
-            &x_axis,
-            canvas::Stroke::default().with_color(iced::Color::WHITE),
-        );
-
+        // X label
         let tx = (self.margin.left + width - self.margin.right) * 0.5;
         let ty = self.margin.bottom * 0.5;
         let text = canvas::Text {
             content: String::from("Occupation annual turnover rate"),
             position: [tx, height - self.margin.bottom + ty].into(),
-            max_width: f32::INFINITY,
-            color: iced::Color::WHITE,
-            size: (12.).into(),
-            line_height: text::LineHeight::default(),
-            font: iced::Font::default(),
+            color: white,
+            size: iced::Pixels(12.),
+            font: iced::Font {
+                weight: iced::font::Weight::Bold,
+                ..Default::default()
+            },
             align_x: iced::Alignment::Center.into(),
             ..Default::default()
         };
         frame.fill_text(text);
 
+        // X axis domain line
+        let start = [self.margin.left, height - self.margin.bottom];
+        let end = [width - self.margin.right, height - self.margin.bottom];
+        frame.stroke(&line(start, end), stroke_white);
+
         for tick in x.ticks(None) {
             let name = format!("{}%", (tick * 100.).round());
             let x_pos = x.apply(tick);
-            let line = canvas::Path::line(
-                [x_pos, height - self.margin.bottom].into(),
-                [x_pos, height - self.margin.bottom + 7.5].into(),
-            );
-            frame.stroke(
-                &line,
-                canvas::Stroke::default().with_color(iced::Color::WHITE),
-            );
 
-            let text = canvas::Text {
+            // Tick lines
+            let start = [x_pos, height - self.margin.bottom];
+            let end = [x_pos, height - self.margin.bottom + 7.5];
+            frame.stroke(&line(start, end), stroke_white);
+
+            // Tick labels
+            frame.fill_text(canvas::Text {
                 content: name,
                 position: [x_pos, height - self.margin.bottom + 8.].into(),
-                max_width: f32::INFINITY,
-                color: iced::Color::WHITE,
-                size: (10.).into(),
-                line_height: text::LineHeight::default(),
-                font: iced::Font::default(),
+                color: white,
+                size: iced::Pixels(10.),
                 align_x: iced::Alignment::Center.into(),
                 ..Default::default()
-            };
-            frame.fill_text(text);
+            });
+
+            // Grid lines
+            let start = [x_pos, self.margin.top];
+            let end = [x_pos, height - self.margin.bottom];
+            frame.stroke(&line(start, end), stroke_white);
         }
 
-        // y axis
-        let y_axis = canvas::Path::line(
-            [self.margin.left, self.margin.top].into(),
-            [self.margin.left, height - self.margin.bottom].into(),
-        );
-        frame.stroke(
-            &y_axis,
-            canvas::Stroke::default().with_color(iced::Color::WHITE),
-        );
-
+        // Y label
         let text = canvas::Text {
             content: String::from("Median wage, 2018"),
             position: [0., 0.].into(),
-            max_width: f32::INFINITY,
             color: iced::Color::WHITE,
-            size: (12.).into(),
-            line_height: text::LineHeight::default(),
-            font: iced::Font::default(),
+            size: iced::Pixels(12.),
+            font: iced::Font {
+                weight: iced::font::Weight::Bold,
+                ..Default::default()
+            },
             align_x: iced::Alignment::Center.into(),
             ..Default::default()
         };
 
+        // Rotate the text (Y label)
         frame.with_save(|frame| {
             frame.rotate(-std::f32::consts::PI * 0.5);
 
@@ -201,59 +193,38 @@ impl<Message> canvas::Program<Message> for Plot {
             frame.fill_text(text);
         });
 
+        // Y axis domain line
+        let start = [self.margin.left, self.margin.top];
+        let end = [self.margin.left, height - self.margin.bottom];
+        frame.stroke(&line(start, end), stroke_white);
+
         for tick in y.ticks(Some(5)) {
             let name = format!("${}k", (tick / 1000.).round());
             let y_pos = y.apply(tick);
-            let line = canvas::Path::line(
-                [self.margin.left - 7.5, y_pos].into(),
-                [self.margin.left, y_pos].into(),
-            );
-            frame.stroke(
-                &line,
-                canvas::Stroke::default().with_color(iced::Color::WHITE),
-            );
 
-            let text = canvas::Text {
+            // Tick lines
+            let start = [self.margin.left - 7.5, y_pos];
+            let end = [self.margin.left, y_pos];
+            frame.stroke(&line(start, end), stroke_white);
+
+            // Tick labels
+            frame.fill_text(canvas::Text {
                 content: name,
                 position: [self.margin.left - 8., y_pos].into(),
-                max_width: f32::INFINITY,
-                color: iced::Color::WHITE,
-                size: (10.).into(),
-                line_height: text::LineHeight::default(),
-                font: iced::Font::default(),
+                color: white,
+                size: iced::Pixels(10.),
                 align_x: iced::Alignment::End.into(),
                 align_y: iced::Alignment::Center.into(),
                 ..Default::default()
-            };
-            frame.fill_text(text);
+            });
+
+            // Grid lines
+            let start = [self.margin.left, y_pos];
+            let end = [width - self.margin.right, y_pos];
+            frame.stroke(&line(start, end), stroke_white);
         }
 
-        // grid
-        for tick in x.ticks(None) {
-            let x_pos = x.apply(tick);
-            let line = canvas::Path::line(
-                [x_pos, self.margin.top].into(),
-                [x_pos, height - self.margin.bottom].into(),
-            );
-            frame.stroke(
-                &line,
-                canvas::Stroke::default().with_color(iced::Color::WHITE),
-            );
-        }
-
-        for tick in y.ticks(Some(5)) {
-            let y_pos = y.apply(tick);
-            let line = canvas::Path::line(
-                [self.margin.left, y_pos].into(),
-                [width - self.margin.right, y_pos].into(),
-            );
-            frame.stroke(
-                &line,
-                canvas::Stroke::default().with_color(iced::Color::WHITE),
-            );
-        }
-
-        // circles
+        // Circles
         for idx in 0..self.turnover.len() {
             let cx = x.apply(self.turnover[idx]);
             let cy = y.apply(self.median_wage[idx]);
@@ -267,7 +238,7 @@ impl<Message> canvas::Program<Message> for Plot {
                 canvas::Fill {
                     style: canvas::Style::Solid(
                         iced::Color::from_str(fill_color)
-                            .unwrap_or(iced::Color::WHITE)
+                            .unwrap_or_default()
                             .scale_alpha(0.5),
                     ),
                     rule: canvas::fill::Rule::EvenOdd,
@@ -278,21 +249,18 @@ impl<Message> canvas::Program<Message> for Plot {
                 &circle,
                 canvas::Stroke::default()
                     .with_width(0.75)
-                    .with_color(iced::Color::from_str(stroke_color).unwrap_or(iced::Color::WHITE)),
+                    .with_color(iced::Color::from_str(stroke_color).unwrap_or_default()),
             );
         }
 
-        // yref
-        let line = canvas::Path::line(
-            [self.margin.left, y.apply(33_900.0)].into(),
-            [width - self.margin.right, y.apply(33_900.0)].into(),
-        );
-
+        // Y reference (horizontal line)
+        let start = [self.margin.left, y.apply(33_900.0)];
+        let end = [width - self.margin.right, y.apply(33_900.0)];
         frame.stroke(
-            &line,
+            &line(start, end),
             canvas::Stroke::default().with_width(1.5).with_color(
                 iced::Color::from_str("#666")
-                    .unwrap_or(iced::Color::WHITE)
+                    .unwrap_or_default()
                     .scale_alpha(0.75),
             ),
         );
