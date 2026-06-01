08a373f7a60c27d1da3f04f456c73259659d48f0
bourbonut <ben.bourbon06@gmail.com>
2026-06-01 19:07:14 +0200

    refactor(scale): Refactor `ticks` and `nice` methods for log transformations

--- src/scale/log.rs
+++ src/scale/log.rs
@@ -1,10 +1,7 @@
 use std::f32;
 use std::mem::swap;
 
-use super::{
-    continuous::Transformer,
-    ticks::{Tick, ticks},
-};
+use super::{continuous::Transformer, ticks::Tick};
 
 /// Logarithm transformation (base `10`)
 #[derive(Clone, Copy)]
@@ -84,112 +81,110 @@ impl Log {
     }
 }
 
+fn ticks(trans: &impl Transformer, base: f32, domain: &[f32; 2], count: usize) -> Vec<f32> {
+    let &[mut u, mut v] = domain;
+    let reverse = v < u;
+
+    if reverse {
+        swap(&mut u, &mut v);
+    }
+
+    let sign = if u > 0. { 1. } else { -1. };
+    let transform = |x| sign * trans.transform(sign * x);
+    let untransform = |x| sign * trans.untransform(sign * x);
+
+    let i = transform(u);
+    let j = transform(v);
+    let n = count;
+    let mut z = Vec::new();
+    if base % 1. == 0. && j - i < n as f32 {
+        let base = base as usize;
+        let start = i.floor() as i32;
+        let end = j.ceil() as i32;
+        if u > 0. {
+            for i in start..end + 1 {
+                for k in 1..base {
+                    let t = if i < 0 {
+                        k as f32 / untransform(-(i as f32))
+                    } else {
+                        k as f32 * untransform(i as f32)
+                    };
+                    if t < u {
+                        continue;
+                    }
+                    if t > v {
+                        break;
+                    }
+                    z.push(t);
+                }
+            }
+        } else {
+            for i in start..end + 1 {
+                for k in (1..base).rev() {
+                    let t = if i > 0 {
+                        k as f32 / untransform(-(i as f32))
+                    } else {
+                        k as f32 * untransform(i as f32)
+                    };
+                    if t < u {
+                        continue;
+                    }
+                    if t > v {
+                        break;
+                    }
+                    z.push(t);
+                }
+            }
+        }
+        if z.len() * 2 < n {
+            z = super::ticks::ticks(u, v, n);
+        }
+    } else {
+        z = super::ticks::ticks(i, j, ((j - i) as usize).min(n))
+            .into_iter()
+            .map(|y| trans.untransform(y))
+            .collect();
+    }
+
+    if reverse {
+        z.reverse();
+    }
+    z
+}
+
+fn nice(trans: &impl Transformer, domain: &[f32; 2], _: usize) -> [f32; 2] {
+    let &[mut x0, mut x1] = domain;
+    let reverse = x1 < x0;
+    if reverse {
+        swap(&mut x0, &mut x1);
+    }
+
+    x0 = if x0 == 0. {
+        x0
+    } else if x0 > 0. {
+        trans.untransform(trans.transform(x0).floor())
+    } else {
+        -trans.untransform(-(-trans.transform(-x0)).floor())
+    };
+    x1 = if x1 == 0. {
+        x1
+    } else if x1 > 0. {
+        trans.untransform(trans.transform(x1).ceil())
+    } else {
+        -trans.untransform(-(-trans.transform(-x1)).ceil())
+    };
+    if reverse { [x1, x0] } else { [x0, x1] }
+}
+
 macro_rules! impl_tick {
     ($type_name:ty) => {
         impl Tick for $type_name {
             fn ticks(&self, domain: &[f32; 2], count: usize) -> Vec<f32> {
-                let &[mut u, mut v] = domain;
-                let reverse = v < u;
-
-                if reverse {
-                    swap(&mut u, &mut v);
-                }
-
-                // TODO: Make a structure or enum to avoid Box
-                let transform: Box<dyn Fn(f32) -> f32> = if u > 0. {
-                    Box::new(|x: f32| self.transform(x))
-                } else {
-                    Box::new(|x: f32| -self.transform(-x))
-                };
-
-                let untransform: Box<dyn Fn(f32) -> f32> = if u > 0. {
-                    Box::new(|x: f32| self.untransform(x))
-                } else {
-                    Box::new(|x: f32| -self.untransform(-x))
-                };
-
-                let i = transform(u);
-                let j = transform(v);
-                let n = count;
-                let mut z = Vec::new();
-                let base = self.base();
-                if self.base() % 1. == 0. && j - i < n as f32 {
-                    let base = base as usize;
-                    let start = i.floor() as i32;
-                    let end = j.ceil() as i32;
-                    if u > 0. {
-                        for i in start..end + 1 {
-                            for k in 1..base {
-                                let t = if i < 0 {
-                                    k as f32 / untransform(-(i as f32))
-                                } else {
-                                    k as f32 * untransform(i as f32)
-                                };
-                                if t < u {
-                                    continue;
-                                }
-                                if t > v {
-                                    break;
-                                }
-                                z.push(t);
-                            }
-                        }
-                    } else {
-                        for i in start..end + 1 {
-                            for k in (1..base).rev() {
-                                let t = if i > 0 {
-                                    k as f32 / untransform(-(i as f32))
-                                } else {
-                                    k as f32 * untransform(i as f32)
-                                };
-                                if t < u {
-                                    continue;
-                                }
-                                if t > v {
-                                    break;
-                                }
-                                z.push(t);
-                            }
-                        }
-                    }
-                    if z.len() * 2 < n {
-                        z = ticks(u, v, n);
-                    }
-                } else {
-                    z = ticks(i, j, ((j - i) as usize).min(n))
-                        .into_iter()
-                        .map(|y| self.untransform(y))
-                        .collect();
-                }
-
-                if reverse {
-                    z.reverse();
-                }
-                z
+                ticks(self, self.base(), domain, count)
             }
 
-            fn nice(&self, domain: &[f32; 2], _: usize) -> [f32; 2] {
-                let &[mut x0, mut x1] = domain;
-                let reverse = x1 < x0;
-                if reverse {
-                    swap(&mut x0, &mut x1);
-                }
-
-                x0 = if x0 == 0. {
-                    x0
-                } else if x0 > 0. {
-                    self.untransform(self.transform(x0).floor())
-                } else {
-                    -self.untransform(-(-self.transform(-x0)).floor())
-                };
-                x1 = if x1 == 0. {
-                    x1
-                } else if x1 > 0. {
-                    self.untransform(self.transform(x1).ceil())
-                } else {
-                    -self.untransform(-(-self.transform(-x1)).ceil())
-                };
-                if reverse { [x1, x0] } else { [x0, x1] }
+            fn nice(&self, domain: &[f32; 2], count: usize) -> [f32; 2] {
+                nice(self, domain, count)
             }
         }
     };
